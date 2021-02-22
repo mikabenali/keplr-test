bd6b99429beb6f6149eb42cdf05f24fce809ba3f
Michael Ben Ali <michaelbenali@macbook-pro-de-michael-2.home>
2021-02-22 19:09:26 +0100

    Add text body-parser

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import morgan from 'morgan'
+import bodyParser from "body-parser";
 import route from './routes'
 
 const app = express();
 const port = process.env.API_PORT || 3000;
 
 app.use(morgan('dev'))
+app.use(bodyParser.text())
 app.use(route);
 
 app.listen(port, () => {
--- src/controllers/ticket.controller.ts
+++ src/controllers/ticket.controller.ts
@@ -1,5 +1,6 @@
 import {Request, Response} from 'express'
 
 export const addTicket = (req: Request, res: Response) => {
+   console.log(req.body)
    return res.sendStatus(200);
 }
